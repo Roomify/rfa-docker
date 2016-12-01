e325933694a11ba46f0eae844019c0d5c5070935
Adrian Rollett <acrollet@gmail.com>
2016-12-01 17:30:07 -0600

    Preserve database between runs

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,9 @@ if [ -z "$DRUPAL_DB_HOST" ]; then
   exit 1
 fi
 
-# if we're linked to MySQL, and we're using the root user, and our linked
-# container has a default "root" password set up and passed through... :)
-: ${DRUPAL_DB_USER:=root}
-if [ "$DRUPAL_DB_USER" = 'root' ]; then
-  : ${DRUPAL_DB_PASSWORD:=$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
-fi
 : ${DRUPAL_DB_NAME:=drupal}
+: ${DRUPAL_DB_USER:=drupal}
+: ${DRUPAL_DB_PASSWORD:=$MYSQL_ENV_MYSQL_PASSWORD}
 
 if [ -z "$DRUPAL_DB_PASSWORD" ]; then
   echo >&2 'error: missing required DRUPAL_DB_PASSWORD environment variable'
@@ -45,11 +41,22 @@ set_config 'DB_NAME' "$DRUPAL_DB_NAME"
 
 chown -R www-data:www-data sites
 
+until nc -z -v -w30 ${DRUPAL_DB_HOST} 3306
+do
+  echo >&2 "Waiting for database connection..."
+  sleep 2
+done
+echo >&2 "mysql ready"
+
 # Check if the database has been imported.
+echo >&2 "Checking for previously installed database..."
 TABLES=$(mysql -h${DRUPAL_DB_HOST} -u${DRUPAL_DB_USER} -p${DRUPAL_DB_PASSWORD} -e 'SHOW TABLES' ${DRUPAL_DB_NAME})
 if [[ ${TABLES} == '' ]]
 then
+  echo >&2 "Database not yet installed, importing..."
   mysql -h${DRUPAL_DB_HOST} -u${DRUPAL_DB_USER} -p${DRUPAL_DB_PASSWORD} ${DRUPAL_DB_NAME} < /var/www/html/accommodations-database.sql
+  echo >&2 "Imported RfA database."
 fi
 
+echo >&2 "Starting webserver..."
 exec "$@"
